b85fae6ac44829e3c9a570799bef3f7920626d0b
Suryani B M <suryani.bm@excelindia.com>
2021-07-09 13:47:29 +0530

    sample1

---
+++ powershellsample.ps1
@@ -0,0 +1,9 @@
+﻿new-item d:\powershellsample -itemtype directory
+new-item d:\powershellsample1 -itemtype directory
+new-item d:\powershellsample\file1.txt
+new-item d:\powershellsample\file2.txt
+new-item d:\powershellsample\file3.txt
+get-childitem d:\powershellsample | copy-item -destination 'd:\powershellsample1'
+$curDateTime = Get-Date -Format yyyyMMdd
+Get-ChildItem D:\powershellsample -Recurse |
+Rename-Item -NewName {$_.Basename + '_' + $curDateTime + $_.Extension }
